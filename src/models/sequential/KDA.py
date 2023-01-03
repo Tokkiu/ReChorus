15657aa504bba008c921a845b56f284ffa44fea5
Ary <mrgao.ary@gmail.com>
2023-01-04 01:14:46 +0800

    add

--- src/models/sequential/KDA.py
+++ src/models/sequential/KDA.py
@@ -296,6 +296,7 @@ class RelationalDynamicAggregation(nn.Module):
         attention = attention - attention.max()
         attention = attention.masked_fill(valid_mask == 0, -np.inf).softmax(dim=-2)
         # temporal evolution
+        import pdb; pdb.set_trace()
         decay = self.idft_decay(delta_t_n).clamp(0, 1).unsqueeze(1).masked_fill(valid_mask==0, 0.)  # B * 1 * H * R
         attention = attention * decay
         # attentional aggregation of history items
